a534996e081e7343ba53f3a808ad6dad1c480f68
Mark Sellors <github@5vcc.com>
2018-05-24 23:48:31 +0100

    a great many updates

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # simple-healtcheck-api
+
 A really simple API, written in Go, to provide status information about the host system. Useful for monitoring purposes.
+
+## Starting the service
+
+In the simple-healthcheck-api directory, run the following:
+
+```
+./simple-healtcheck-api
+```
+
+## Dashboard
+
+Once started, the service presents a web based dashboard to the user. This is created using the 'www/dashboard.html'.
+
+The dashboard is available by visiting `http://localhost:1234/`.
+
+## API
+
+The dashboard gets its data from the API, which is available at `http://localhost:1234/__healthcheck__`.
+
+The API returns JSON is the following format:
+
+```
+{cpu: 5, load: 1.97, mem: 61.24, version: "0.1"}
+```
--- healthcheck.go
+++ healthcheck.go
@@ -3,14 +3,14 @@ package main
 import(
     "math"
     "time"
-    //"fmt"
     "github.com/shirou/gopsutil/mem"
     "github.com/shirou/gopsutil/cpu"
     "github.com/shirou/gopsutil/load"
     "encoding/json"
-    //"net"
     "net/http"
     "log"
+    "html/template"
+    "os"
 )
 
 type response struct {
@@ -18,6 +18,11 @@ type response struct {
     Load  float64    `json:"load"`
     Mem float64 `json:"mem"`
     Version string `json:"version"`
+}
+
+type Page struct {
+    Title string
+    Body  []byte
 }
 
 func round(num float64) int {
@@ -42,18 +47,33 @@ func statHandler(rw http.ResponseWriter, r *http.Request) {
     //     Mem: v.UsedPercent}
     // fmt.Println(res1)
     //log.Println(res1.cpu)
+    log.Println("returning JSON")
     json.NewEncoder(rw).Encode(response{Cpu: toFixed(c[0], 2),
                                         Load: toFixed(l.Load1, 2),
                                         Mem: toFixed(v.UsedPercent, 2),
                                         Version: version})
 }
+
+func dashHandler(w http.ResponseWriter, r *http.Request) {
+    title, err := os.Hostname()
+    if err != nil {
+		panic(err)
+	}
+    p := &Page{Title: title}
+    t, _ := template.ParseFiles("www/dashboard.html")
+    log.Println("Attempting to render dashboard")
+    t.Execute(w, p)
+}
+
 func main() {
     address := ":1234"
     r := http.NewServeMux()
     r.HandleFunc("/__healthcheck__", statHandler)
+    r.HandleFunc("/", dashHandler)
     log.Println("Healthcheck Service request at: " + address)
+    log.Println("Please visit http://localhost:1234/")
+    //log.Println("hostname: " + os.Hostname())
     log.Println(http.ListenAndServe(address, r))
-    
 
     // mem - almost every return value is a struct
     // fmt.Printf("Mem: %f\n", v.UsedPercent)
